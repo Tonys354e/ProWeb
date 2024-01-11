e6b8fb72df74095a02ba1ad3acc9efc828260880
Antony <202100354e@utea.edu.pe>
2024-01-11 20:40:47 +0100

    commit3

---
+++ programacion2.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title> PHP -HTML5 </title>
+</head>
+
+<body>
+
+ <p>bienvenido a php</p>
+    <?php
+    echo ("hola perro");
+    ?>
+</body>
+
+</html>
